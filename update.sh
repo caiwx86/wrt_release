c93928d28e84018c013e711d01e7eb7c506ff515
caiwx86 <caiwenxiu0806@163.com>
2025-04-13 17:58:12 +0800

    添加netspeedtest和msd_lite

--- update.sh
+++ update.sh
@@ -155,7 +155,8 @@ install_small8() {
         luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest \
         netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash luci-app-homeproxy \
         luci-app-amlogic nikki luci-app-nikki tailscale luci-app-tailscale oaf open-app-filter luci-app-oaf \
-        luci-theme-argon luci-theme-argon-config easytier luci-app-easytier nps luci-app-npc msd_lite luci-app-msd_lite
+        luci-theme-argon luci-theme-argon-config easytier luci-app-easytier nps luci-app-npc msd_lite luci-app-msd_lite \
+        homebox luci-app-netspeedtest
 }
 
 install_feeds() {
